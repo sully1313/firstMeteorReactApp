391f30243c9747d47832613a85e38e3a197c303a
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-06 15:37:28 -0700

    react enviroment setup

--- client/main.js
+++ client/main.js
@@ -1,5 +1,8 @@
-import someDefault from './../imports/utils';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
 
-
-console.log('Log from /client/main.js');
-console.log(someDefault);
+Meteor.startup(function () {
+  let jsx = <p>This is from main.js</p>;
+  ReactDOM.render(jsx, document.getElementById('app'));
+});
--- imports/utils.js
+++ imports/utils.js
@@ -3,7 +3,3 @@ console.log('Log from /imports/utlis.js');
 export let greetUser = function () {
   return 'Hello User!';
 };
-
-export let name = 'Matt Sullivan';
-
-export default 'Default val'; 
