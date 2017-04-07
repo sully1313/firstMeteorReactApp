ccc6cf0b386db8228a9c55e4c786e78d2197791d
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-06 17:35:06 -0700

    rendering array list on dom

--- client/main.js
+++ client/main.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
 Meteor.startup(function () {
-  let jsx = <p>This is from main.js</p>;
+  let title = 'Score Keeper'
+  let name = 'Matt'
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}!</p>
+      <p>This is my second p.</p>
+    </div>
+);
   ReactDOM.render(jsx, document.getElementById('app'));
 });
