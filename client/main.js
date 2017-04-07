8a560de25beb62296677d9cfc9a2b244e4e3907c
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-07 15:26:36 -0700

    refactored to es6 arrow functions

--- client/main.js
+++ client/main.js
@@ -5,13 +5,13 @@ import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
 
-const renderPlayers = function (playersList) {
-  return playersList.map(function (player) {
+const renderPlayers = (playersList) => {
+  return playersList.map((player) => {
     return <p key={player._id}>{player.name} has {player.score} point(s)</p>;
   });
 };
 
-const handleSubmit = function (e) {
+const handleSubmit = (e) => {
   let playerName = e.target.playerName.value;
 
   e.preventDefault();
@@ -25,8 +25,8 @@ const handleSubmit = function (e) {
   }
 };
 
-Meteor.startup(function () {
-  Tracker.autorun(function () {
+Meteor.startup(() => {
+  Tracker.autorun(() => {
     let players = Players.find().fetch();
     let title = 'Score Keeper'
     let name = 'Matt'
--- server/main.js
+++ server/main.js
@@ -1,10 +1,6 @@
 import {Meteor} from 'meteor/meteor'
 import {Players} from './../imports/api/players';
 
-Meteor.startup(function () {
-  Players.insert({
-    name: 'MarBear',
-    score: 13
-  });
-  console.log(Players.find().fetch());
+Meteor.startup(() => {
+
 });
