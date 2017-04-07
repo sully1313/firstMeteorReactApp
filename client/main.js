67f855f967a121c9b548fef1f777e73bb2c3fc99
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-07 14:42:59 -0700

    form submit & create functionality

--- client/main.js
+++ client/main.js
@@ -11,6 +11,20 @@ const renderPlayers = function (playersList) {
   });
 };
 
+const handleSubmit = function (e) {
+  let playerName = e.target.playerName.value;
+
+  e.preventDefault();
+
+  if(playerName) {
+    e.target.playerName.value = '';
+    Players.insert({
+      name: playerName,
+      score: 0
+    });
+  }
+};
+
 Meteor.startup(function () {
   Tracker.autorun(function () {
     let players = Players.find().fetch();
@@ -22,12 +36,12 @@ Meteor.startup(function () {
       <p>Hello {name}!</p>
       <p>This is my second p.</p>
       {renderPlayers(players)}
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="playerName" placeholder="Player's name"/>
+        <button>Add Player</button>
+      </form>
     </div>
   );
     ReactDOM.render(jsx, document.getElementById('app'));
-  });
-  Players.insert({
-    name: 'Jejomar',
-    score: -13
   });
 });
