0598e7931ac7e6d251007f83f2e4310fe7590900
Matt Sullivan <mattbsullivan@gmail.com>
2017-04-08 21:22:51 -0700

    PlayerList react component implemented

--- client/main.js
+++ client/main.js
@@ -6,23 +6,8 @@ import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
 import TitleBar from './../imports/ui/TitleBar';
 import AddPlayer from './../imports/ui/AddPlayer';
+import PlayerList from './../imports/ui/PlayerList';
 
-const renderPlayers = (playersList) => {
-  return playersList.map((player) => {
-    return (
-      <p key={player._id}>
-        {player.name} has {player.score} point(s)
-        <button onClick={() => {
-            Players.update(player._id, {$inc: {score: -1}});
-          }}>-1</button>
-        <button onClick={() => {
-            Players.update(player._id, {$inc: {score: 1}});
-          }}>+1</button>
-        <button onClick={() => Players.remove(player._id)}>X</button>
-      </p>
-    );
-  });
-};
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
@@ -31,7 +16,7 @@ Meteor.startup(() => {
     let jsx = (
     <div>
       <TitleBar title={title} subtitle="created by Matt Sullivan"/>
-      {renderPlayers(players)}
+      <PlayerList players={players}/>
       <AddPlayer/>
     </div>
   );
